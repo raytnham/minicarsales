366b0804be65a4d384d06dd9f9b881e7b9204d15
Ray Nham <ray.t.nham@gmail.com>
2020-11-17 07:30:26 +1100

    Added: Sqlite migration

---
+++ MiniCarsales/Migrations/20201116202106_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MiniCarsales.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Engine = table.Column<string>(type: "TEXT", nullable: false),
+                    Doors = table.Column<int>(type: "INTEGER", nullable: false),
+                    Wheels = table.Column<int>(type: "INTEGER", nullable: false),
+                    BodyType = table.Column<int>(type: "INTEGER", nullable: false),
+                    VehicleType = table.Column<int>(type: "INTEGER", nullable: false),
+                    Make = table.Column<string>(type: "TEXT", nullable: false),
+                    Model = table.Column<string>(type: "TEXT", nullable: false),
+                    Year = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
---
+++ MiniCarsales/Models/CarsalesContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MiniCarsales.Models
+{
+	public class CarsalesContext : DbContext
+	{
+		public DbSet<Car> Cars { get; set; }
+		protected override void OnConfiguring(DbContextOptionsBuilder options)
+			=> options.UseSqlite("Data Source=carsales.db");
+	}
+}
