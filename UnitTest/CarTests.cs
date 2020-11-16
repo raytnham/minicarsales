98c75a9870f1bcd2102e0972834dcd159e9fd58d
Ray Nham <ray.t.nham@gmail.com>
2020-11-17 09:28:24 +1100

    Added: Car's unit tests

--- UnitTest/CarTests.cs
+++ UnitTest/CarTests.cs
@@ -1,10 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiniCarsales.Models;
+using MiniCarsales.Enums;
+using MiniCarsales.Providers;
 
 namespace UnitTest
 {
-	class CarTests
+	[TestClass]
+	public class CarTests
 	{
+		private readonly CarProvider _carProvider = CarProvider.GetInstance();
+
+		[TestMethod]
+		public void CreateCarSuccessfully()
+		{
+			var validCar = new Car
+			{
+				VehicleType = VehicleType.Car,
+				Make = "Toyota",
+				Model = "Camry",
+				Year = 2015,
+				Engine = "Inline",
+				Doors = 4,
+				Wheels = 4,
+				BodyType = BodyType.Sedan
+			};
+			var result = _carProvider.AddCar(validCar, false);
+			Assert.IsTrue(result);
+		}
+
+		[TestMethod]
+		public void CreateCarFailed()
+		{
+			var invalidCar = new Car
+			{
+				VehicleType = VehicleType.Car,
+				Make = string.Empty,
+				Model = string.Empty,
+				Year = 2015,
+				Engine = "Inline",
+				Doors = 4,
+				Wheels = 4,
+				BodyType = BodyType.Sedan
+			};
+			var result = _carProvider.AddCar(invalidCar);
+			Assert.IsFalse(result);
+		}
 	}
 }
