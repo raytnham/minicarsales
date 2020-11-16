7b5149cba26b4ac997efee4acc4d19fa1f83f787
Ray Nham <ray.t.nham@gmail.com>
2020-11-17 06:47:39 +1100

    Project init

---
+++ MiniCarsales/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MiniCarsales.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
